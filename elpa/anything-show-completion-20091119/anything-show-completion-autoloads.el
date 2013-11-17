88eff6a7341c45fab491dcad6a1c5ce60c926a9d
Satoshi Takano <satoxi.tacano@gmail.com>
2013-11-17 20:52:52 +0900

    Use emacs package manager

---
+++ elpa/anything-show-completion-20091119/anything-show-completion-autoloads.el
@@ -0,0 +1,18 @@
+;;; anything-show-completion-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("anything-show-completion-pkg.el" "anything-show-completion.el")
+;;;;;;  (21128 36303 428236 0))
+
+;;;***
+
+(provide 'anything-show-completion-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anything-show-completion-autoloads.el ends here
--- init.el
+++ init.el
@@ -1,6 +1,14 @@
-;; general
 (add-to-list 'load-path "~/.emacs.d")
-(add-to-list 'load-path "~/.emacs.d/auto-install")
+
+(package-initialize)
+(add-to-list
+  'package-archives
+   '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list
+  'package-archives
+   '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+
 
 (setq inhibit-startup-message t)
 (setq make-backup-files nil)
@@ -12,15 +20,6 @@
 (setq-default tab-width 2)
 (setq-default case-fold-search nil)
 
-;; hide toolbar when using MacEmacs
-(cond 
- ((equal window-system 'ns)
-  (dolist (conf (list 
-                 "cocoa-font.el"))
-    (load (concat user-emacs-directory "conf/cocoa/" conf))))
- ((equal window-system nil)
-  ;; do something
-  ))
 (line-number-mode t)
 (column-number-mode t)
 
@@ -32,11 +31,6 @@
 (setq default-frame-alist initial-frame-alist)
 (setq hl-line-face 'hlline-face)
 (global-hl-line-mode)
-(add-to-list 'default-frame-alist '(alpha . 85))
-;; tool-bar off
-;;(tool-bar-mode 0)
-;; scroll-bar off
-;;(scroll-bar-mode 0)
 
 (global-set-key "\C-m" 'newline-and-indent)
 (global-set-key "\C-h" 'backward-delete-char)
@@ -45,148 +39,42 @@
 (global-set-key "\M-c" 'comment-region)
 (define-key global-map (kbd "C-t") 'next-multiframe-window)
 
-;; window resizing
-(global-set-key [?\C-{] 'shrink-window-horizontally)
-(global-set-key [?\C-}] 'enlarge-window-horizontally)
-(global-set-key [?\C-\"] 'shrink-window)
-(global-set-key [?\C-+] 'enlarge-window)
-
-;;kill-ring
-(setq kill-ring-max 20)
-;;anything で対象とするkill-ring の要素の長さの最小値.
-;;デフォルトは 10.
-(setq anything-kill-ring-threshold 5)
-(global-set-key "\M-y" 'anything-show-kill-ring)
- 
-;; wb-line-number
-;;(require 'wb-line-number)
-
-;; undo-tree
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-;; yasnippet
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/snippets")
-
-;; auto-install
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/auto-install/")
-;;(auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
-
-;; anything
-;; (require 'anything-startup)
+;; color
+(custom-set-variables
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass))))
 
 ;; auto-complete
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
 (ac-config-default)
 
-;; ruby-mode
-(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
-(setq auto-mode-alist (cons '("\\.rb$" . ruby-mode) auto-mode-alist))
-(setq interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook '(lambda () (inf-ruby-keys)))
-(setq ruby-deep-indent-paren-style nil)
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (inf-ruby-keys)
-             (require 'ruby-electric)
-             (ruby-electric-mode t)
-             (define-key ruby-mode-map "\C-m" 'newline-and-indent)
-             (define-key ruby-mode-map "\C-c\C-z" 'run-ruby)
-             ))
-
-;; rubydb
-(autoload 'ruby "rubydb2x"
-	  "run rubydb on program file in buffer *gud-file*.
-the directory containing file becomes the initial working directory
-and source-file directory for your debugger." t)
-
-(setq ruby-indent-level 2)
-(setq ruby-indent-tabs-mode nil)
-
-
-;; less-css-mode
-(require 'less-css-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (wheatgrass))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
- (autoload 'js2-mode "js2" nil t)
- (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
- (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
-
- ;; js2-mode
-(when (load "js2" t)
-  (setq js2-cleanup-whitespace nil
-        js2-mirror-mode nil
-        js2-bounce-indent-flag nil)
-
-  (defun indent-and-back-to-indentation ()
-    (interactive)
-    (indent-for-tab-command)
-    (let ((point-of-indentation
-           (save-excursion
-             (back-to-indentation)
-             (point))))
-      (skip-chars-forward "\s " point-of-indentation)))
-
-  (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
-  (define-key js2-mode-map "\C-m" nil))
-
-;; mmm-mode
-(require 'mmm-mode)
-(setq mmm-global-mode 'maybe)
-(setq mmm-submode-decoration-level 3)
-(set-face-background 'mmm-default-submode-face "black")
-
-(mmm-add-classes
- '((embedded-css
-    :submode css-mode
-    :front "<style[^>]*>"
-    :back "</style>")))
-(mmm-add-mode-ext-class nil "\\.html?\\'" 'embedded-css)
-(mmm-add-mode-ext-class nil "\\.erb?\\'" 'embedded-css)
-
-(mmm-add-classes
- '((html-javascript
-    :submode javascript-mode
-    :front "<script[^>]*>"
-    :back "</script>")))
-(mmm-add-mode-ext-class nil "\\.html?\\'" 'html-javascript)
-(mmm-add-mode-ext-class nil "\\.erb?\\'" 'html-javascript)
+;; dropdown-list
+(require 'dropdown-list)
 
 ;; nav
 (require 'nav)
-(setq nav-split-window-direction 'vertical) ;; 分割したフレームを垂直に並べる
-(global-set-key "\C-x\C-d" 'nav-toggle)     ;; C-x C-d で nav をトグル
+(setq nav-split-window-direction 'vertical)
+(global-set-key "\C-x\C-d" 'nav-toggle)
 
+;; wb-line-number
+(require 'wb-line-number)
+(global-set-key "\C-l" 'wb-line-number-toggle)
 
-;; scss-mode
-(autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-(setq scss-compile-at-save nil)
+;;kill-ring
+(global-set-key "\M-y" 'anything-show-kill-ring)
+ 
+;; undo-tree
+(require 'undo-tree)
+(global-undo-tree-mode 1)
 
+;; anything
+(require 'anything)
+(require 'anything-startup)
 
-;; perl-completion
-(setq auto-mode-alist (cons '("\\.pl$" . cperl-mode) auto-mode-alist))
-(add-hook  'cperl-mode-hook (lambda ()
-                              (require 'auto-complete)
-                              (require 'perl-completion)
-                              (add-to-list 'ac-sources 'ac-source-perl-completion)
-                              (perl-completion-mode t)))
+;; yasnippet
+(require 'yasnippet)
+(yas--initialize);
+(yas-global-mode 1)
+
+;; js-mode
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
