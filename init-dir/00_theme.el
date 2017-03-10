24e497ee438c012ca8ccbb8cbe0f9d833df4cb19
satoshi-takano <satoxi.tacano@gmail.com>
2017-03-10 12:09:07 +0900

    update theme

--- init-dir/00_theme.el
+++ init-dir/00_theme.el
@@ -1,13 +1,13 @@
 ;; load theme
-(load-theme 'misterioso t)
+(load-theme 'tango-dark t)
 (setq sml/theme 'dark)
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
 
 (custom-set-faces
  '(magit-diff-added ((t (:background "black" :foreground "#00ff00"))))
- '(magit-diff-added-highlight ((t (:background "#555555" :foreground "#00ff00"))))
+ '(magit-diff-added-highlight ((t (:background "#303030" :foreground "#00ff00"))))
  '(magit-diff-removed ((t (:background "black" :foreground "#ff0000"))))
- '(magit-diff-removed-highlight ((t (:background "#555555" :foreground "#ff0000"))))
+ '(magit-diff-removed-highlight ((t (:background "#303030" :foreground "#ff0000"))))
  '(magit-hash ((t (:foreground "#ff0000"))))
 )
--- init.el
+++ init.el
@@ -20,13 +20,3 @@
  '(package-selected-packages
    (quote
     (highlight-symbol helm-projectile go-mode swift-mode zenburn-theme yasnippet undo-tree smart-mode-line magit init-loader helm expand-region auto-complete ada-mode ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(magit-diff-added ((t (:background "black" :foreground "#00ff00"))))
- '(magit-diff-added-highlight ((t (:background "#555555" :foreground "#00ff00"))))
- '(magit-diff-removed ((t (:background "black" :foreground "#ff0000"))))
- '(magit-diff-removed-highlight ((t (:background "#555555" :foreground "#ff0000"))))
- '(magit-hash ((t (:foreground "#ff0000")))))
